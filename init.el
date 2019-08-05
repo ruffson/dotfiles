696fc982d07dd6210833ad2729e43633ebdc8f5a
Raphael Tessmer <raphael@micropsi-industries.com>
2019-08-05 13:57:20 +0200

    Enable ido-mode in emacs

--- init.el
+++ init.el
@@ -32,6 +32,11 @@
 (load-theme 'dracula t) ;; load dracula theme
 (global-linum-mode t) ;; enable line numbers globally
 
+;; enable ido-mode
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
