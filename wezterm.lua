d1613f7957d5d1cd82d9b5f5a0ab9d7adc58e6ba
ruffson <ruffson@mailbox.org>
2021-05-27 19:44:51 +0200

    Update kitty font settings

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,15 @@
 local wezterm = require 'wezterm';
 
 return {
-  -- color_scheme = "Grape",
-  color_scheme = "MaterialOcean",
+  color_scheme = "Grape",
+  -- color_scheme = "MaterialOcean",
   -- font = wezterm.font("JetBrainsMono Nerd Font"),
   font = wezterm.font("CaskaydiaCove Nerd Font"),
-  font_size = 14,
+  font_size = 13,
   warn_about_missing_glyphs = false,
   window_decorations = "NONE",
   enable_tab_bar = false,
   enable_csi_u_key_encoding = true,
   initial_rows = 40,
-  initial_cols = 100,
+  initial_cols = 85,
 }
