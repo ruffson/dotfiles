2ebe1738c33d4971fb6fbcec294abf04d55d3ee4
ruffson <ruffson@mailbox.org>
2022-11-28 19:44:33 -0800

    Update API calls

--- init.lua
+++ init.lua
@@ -334,7 +334,7 @@ cmp.setup.cmdline(":", {
 })
 
 -- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local nvim_lsp = require("lspconfig")
 
 -- Add on_attach to EACH server's setup fn
