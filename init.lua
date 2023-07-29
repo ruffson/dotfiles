a2282976b2431973d87f040ef0df83ec879586ad
ruffson <ruffson@mailbox.org>
2023-07-29 14:26:35 -0700

    Enable treesitter highlighting

--- init.lua
+++ init.lua
@@ -291,6 +291,25 @@ keymap("n", "<leader>ld", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 -- keymap("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-d>')<cr>")
 
 -- --------------------
+-- Treesitter --
+-- --------------------
+
+require'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true,
+        -- disable for large files
+        disable = function(lang, buf)
+            local max_filesize = 1024 * 1024
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+        end,
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+-- --------------------
 -- LSP/CMP Setup --
 -- --------------------
 
