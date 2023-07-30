015e343330ca4e706ef735fbf07f7d8d904e677a
ruffson <ruffson@mailbox.org>
2023-07-29 23:51:45 -0700

    Remove restriction on treesitter highlight

--- init.lua
+++ init.lua
@@ -295,17 +295,10 @@ keymap("n", "<leader>ld", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
 -- --------------------
 
 require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "lua" },
     highlight = {
-        enable = true,
-        -- disable for large files
-        disable = function(lang, buf)
-            local max_filesize = 1024 * 1024
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-        end,
-        additional_vim_regex_highlighting = false,
+      enable = true,
+      additional_vim_regex_highlighting = false,
     },
 }
 
