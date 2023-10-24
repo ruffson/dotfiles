5932076fb0155856b6cb355680050620a9ba9487
ruffson <ruffson@mailbox.org>
2023-10-24 09:14:16 -0700

    Disable cmp suggestions in comments

--- init.lua
+++ init.lua
@@ -388,9 +388,19 @@ cmp.setup({
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
-    }, {
         { name = "buffer" },
     }),
+    enabled = function()
+        -- disable completion in comments
+        local context = require 'cmp.config.context'
+         -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == 'c' then
+            return true
+        else
+            return not context.in_treesitter_capture("comment") 
+                and not context.in_syntax_group("Comment")
+        end
+    end
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
