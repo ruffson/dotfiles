9ecee16738963505c8079700639df0ee653867c4
ruffson <ruffson@mailbox.org>
2024-02-22 14:04:27 -0800

    Add basic fish config

---
+++ fish/basic_config.fish
@@ -0,0 +1,28 @@
+# >>> Start TMUX on login >>>
+if status is-interactive
+    and not set -q TMUX
+    exec tmux
+end
+# <<< Start TMUX on login <<<
+
+# >>> ALIASES >>>
+abbr -a g git
+abbr -a vim nvim
+abbr -a ls lsd
+abbr -a l lsd -l
+abbr -a ll lsd -l
+abbr -a ip ip -c
+abbr -a grep rg
+# <<< ALIASES <<<
+
+# >>> Exports >>>
+set -gx VISUAL "nvim"
+set -gx EDITOR "nvim"
+set -gx FZF_DEFAULT_COMMAND "fd --type f"
+set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -gx MANROFFOPT "-c"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.cargo/bin"
+# fish_add_path "$HOME/Applications/mambaforge/bin"
+# <<< Exports <<<
+zoxide init fish | source
