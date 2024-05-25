c0551f5e4645991396b5b96077bb961e00e97703
ruffson <ruffson@mailbox.org>
2024-05-24 20:26:55 -0700

    Remove neorg

--- nvim_config/init.lua
+++ nvim_config/init.lua
@@ -471,24 +471,6 @@ require("rust-tools").setup({
 -- local coq = require("coq")
 
 -- --------------------
--- NEORG --
--- --------------------
-
-require("neorg").setup {
-    load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-                workspaces = {
-                    notes = "~/Documents/neorg",
-                },
-                default_workspace = "notes",
-            },
-        },
-    },
-}
--- --------------------
 -- Neodev --
 -- --------------------
 require("neodev").setup({
