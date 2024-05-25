6f41107ffb30a9ebff2c7a67ba6882f2595c04e1
ruffson <ruffson@mailbox.org>
2024-05-24 20:27:08 -0700

    Add copilot

--- nvim_config/lua/plugins.lua
+++ nvim_config/lua/plugins.lua
@@ -128,23 +128,55 @@ require("lazy").setup({
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
-    {
-        "m4xshen/hardtime.nvim",
-        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-        opts = {},
-    },
     -- {
     --     "zbirenbaum/copilot.lua",
     --     config = function() require("copilot").setup() end,
     -- },
     {
         "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        build = ":Copilot auth",
+        lazy = false,
         opts = {
-            suggestion = { enabled = true },
-            panel = { enabled = false },
-            auto_trigger = { enabled = true },
+            panel = {
+               enabled = true,
+               auto_refresh = false,
+               keymap = {
+                 jump_prev = "[[",
+                 jump_next = "]]",
+                 accept = "<CR>",
+                 refresh = "gr",
+                 open = "<M-CR>"
+               },
+               layout = {
+                 position = "bottom", -- | top | left | right
+                 ratio = 0.4
+               },
+            },
+            suggestion = {
+               enabled = true,
+               auto_trigger = true,
+               debounce = 75,
+               keymap = {
+                 accept = "<M-l>",
+                 accept_word = false,
+                 accept_line = false,
+                 next = "<M-]>",
+                 prev = "<M-[>",
+                 dismiss = "<C-]>",
+               },
+            },
+            filetypes = {
+               yaml = false,
+               markdown = false,
+               help = false,
+               gitcommit = false,
+               gitrebase = false,
+               hgcommit = false,
+               svn = false,
+               cvs = false,
+               ["."] = false,
+            },
+            copilot_node_command = 'node', -- Node.js version must be > 18.x
+            server_opts_overrides = {},
         },
     },
     -- -------------
