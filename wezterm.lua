f1c118002dee8db071b95f2e1ace1b2087797277
raph <raphael@bright.ai>
2025-04-03 16:14:20 -0700

    Update config

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 return {
-  color_scheme = "Grape",
-  -- color_scheme = "MaterialOcean",
-  -- font = wezterm.font("JetBrainsMono Nerd Font"),
-  font = wezterm.font("Cascadia Code PL"),
-  font_size = 13,
-  warn_about_missing_glyphs = false,
-  window_decorations = "NONE",
-  enable_tab_bar = false,
-  enable_csi_u_key_encoding = true,
-  initial_rows = 40,
-  initial_cols = 85,
+    color_scheme = "Tokyo Night",
+    -- color_scheme = "MaterialOcean",
+    -- font = wezterm.font("JetBrainsMono Nerd Font"),
+    font = wezterm.font("Cascadia Code PL"),
+    font_size = 12,
+    warn_about_missing_glyphs = false,
+    window_decorations = "NONE",
+    enable_tab_bar = false,
+    enable_csi_u_key_encoding = true,
+    initial_rows = 40,
+    initial_cols = 100,
 }
